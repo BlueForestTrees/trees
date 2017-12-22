a7d8da546ce002bf9c1d5f494fcd1b4f31cf6bee
slimane <smedini@gmail.com>
2017-12-22 13:31:38 +0100

    treefy ok

--- src/service/transforms.js
+++ src/service/transforms.js
@@ -3,30 +3,21 @@ const cached = (_id, cache) => _.find(cache, (e => e._id.equals(_id)));
 
 const buildRoots = (coef, childList, cache) => {
     const roots = [];
-
     if (childList) {
         _.forEach(childList, child => {
             const cachon = cached(child._id, cache);
-            const qt = cachon.qt ? cachon.qt * coef : child.qt;
-
-            console.log(child.qt + " / " + qt);
-
+            const qt = coef * child.qt;
             roots.push({
-                _id: cachon._id,
-                name: cachon.name,
-                qt: qt,
-                roots: buildRoots(child.qt / qt, cachon.childList, cache)
+                _id: cachon._id, name: cachon.name, qt: qt,
+                roots: buildRoots(qt / cachon.qt || 1, cachon.childList, cache)
             });
         });
     }
-
     return roots;
 };
 
 const treefy = dbTree => ({
-    _id: dbTree._id,
-    name: dbTree.name,
-    qt: dbTree.qt,
+    _id: dbTree._id, name: dbTree.name, qt: dbTree.qt,
     roots: buildRoots(1, dbTree.childList, dbTree.cache)
 });
 

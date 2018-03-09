0b5a04c346d4ff0a8780556f6850897934dc5a2d
Clement <clement.langendorf@gmail.com>
2018-03-09 13:47:48 +0100

    Création withEntry

--- src/test/database/impactEntries.js
+++ src/test/database/impactEntries.js
@@ -1,11 +1,22 @@
 import {cols} from "../../main/const/collections";
+import {withEntry} from "../testPlumbing";
 
+
+export const vitCImpactEntry = withEntry( "5a6a03c03e77667641d2d2c4", "Ivitamine C", "Densité");
+export const vitBImpactEntry = withEntry( "5a6a03c03e77667641d2d2c5", "Ivitamine B", "Densité");
+export const prixImpactEntry = withEntry( "5a6a03c03e77667641d2d2c6", "IPrix", "Coût");
+export const vitDImpactEntry = withEntry( "5a6a03c03e77667641d2d2c7", "Ivitamine D",  "Densité");
+export const vitEImpactEntry = withEntry( "5a6a03c03e77667641d2d2c8", "Ivitamine E",  "Densité");
+export const co2eImpactEntry = withEntry( "5a6a03c03e77667641d2d2c9", "équivalent CO2",  "Masse");
+
+/*
 export const vitCImpactEntry = {_id: "5a6a03c03e77667641d2d2c4", name: "Ivitamine C", grandeur: "Densité"};
 export const vitBImpactEntry = {_id: "5a6a03c03e77667641d2d2c5", name: "Ivitamine B", grandeur: "Densité"};
 export const prixImpactEntry = {_id: "5a6a03c03e77667641d2d2c6", name: "IPrix", grandeur: "Coût"};
 export const vitDImpactEntry = {_id: "5a6a03c03e77667641d2d2c7", name: "Ivitamine D", grandeur: "Densité"};
 export const vitEImpactEntry = {_id: "5a6a03c03e77667641d2d2c8", name: "Ivitamine E", grandeur: "Densité"};
 export const co2eImpactEntry = {_id: "5a6a03c03e77667641d2d2c9", name: "équivalent CO2", grandeur: "Masse"};
+*/
 
 export const database = {
     [cols.IMPACT_ENTRY]: [vitDImpactEntry, vitEImpactEntry, co2eImpactEntry, vitCImpactEntry, vitBImpactEntry, prixImpactEntry]

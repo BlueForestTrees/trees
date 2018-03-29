dd122590766100119177f22b5af63389713df7f1
slim <smedini@gmail.com>
2018-03-29 11:35:38 +0200

    env => arguments

--- src/env.js
+++ src/env.js
@@ -1,10 +1,12 @@
 import {debug} from "./main/util/debug";
 
+const cmdArgs = require("args-parser")(process.argv);
+
 const ENV = {
-    PORT: process.env.PORT || 8080,
-    DB_NAME: process.env.DB_NAME || "BlueForestTreesDB",
-    DB_HOST: process.env.DB_HOST || "localhost",
-    DB_PORT: process.env.DB_PORT || 27017,
+    PORT: cmdArgs.port || process.env.PORT || 8080,
+    DB_NAME: cmdArgs.dbName || process.env.DB_NAME || "BlueForestTreesDB",
+    DB_HOST: cmdArgs.dbHost || process.env.DB_HOST || "localhost",
+    DB_PORT: cmdArgs.dbPort || process.env.DB_PORT || 27017,
     NODE_ENV: process.env.NODE_ENV || null,
     VERSION: process.env.npm_package_version
 };

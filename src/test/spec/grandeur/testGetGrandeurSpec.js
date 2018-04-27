753758827b7638f69221de451285f3a4aa8116f9
slim <smedini@gmail.com>
2018-04-27 15:36:03 +0200

    add facet entry && facet

--- src/test/spec/grandeur/testGetGrandeurSpec.js
+++ src/test/spec/grandeur/testGetGrandeurSpec.js
@@ -3,74 +3,182 @@ export const getGrandeurSpec = {};
 getGrandeurSpec.res = {
     body: [
         {
-            "key": "Ener",
-            "label": "Energie (wh, J, cal...)",
-            "units": [
-                {
-                    "coef": 0.23923445,
-                    "grandeur": "Ener",
-                    "name": "joule",
-                    "shortname": "J"
-                },
-                {
-                    "coef": 0.23923445,
-                    "grandeur": "Ener",
-                    "name": "Watt-Seconde",
-                    "shortname": "ws"
-                },
-                {
-                    "coef": 1,
-                    "grandeur": "Ener",
-                    "name": "calorie",
-                    "shortname": "cal"
-                },
-                {
-                    "coef": 861.24402,
-                    "grandeur": "Ener",
-                    "name": "Watt-Heure",
-                    "shortname": "wh"
-                },
-                {
-                    "coef": 861244.02,
-                    "grandeur": "Ener",
-                    "name": "Watt-Heure",
-                    "shortname": "kwh"
-                },
-                {
-                    "coef": 1000,
-                    "grandeur": "Ener",
-                    "name": "kilo-calorie",
-                    "shortname": "kcal"
-                },
-                {
-                    "coef": 1000000,
-                    "grandeur": "Ener",
-                    "name": "mega-calorie",
-                    "shortname": "Mcal"
-                }
-            ]
-        },
-        {
             "key": "Dens",
             "label": "Densité (mol, mmol...)",
             "units": [
                 {
                     "coef": 0.000001,
                     "grandeur": "Dens",
-                    "name": "micro-mole",
+                    "name": "Micro-mole (umol)",
                     "shortname": "μmol"
                 },
                 {
                     "coef": 0.001,
                     "grandeur": "Dens",
-                    "name": "milli-mole",
+                    "name": "Milli-mole (mmol)",
                     "shortname": "mmol"
                 },
                 {
                     "coef": 1,
                     "grandeur": "Dens",
-                    "name": "mole",
+                    "name": "Mole (mol)",
                     "shortname": "mol"
+                }
+            ]
+        },
+        {
+            "key": "Duré",
+            "label": "Durée (sec, min, h...)",
+            "units": [
+                {
+                    "coef": 1,
+                    "grandeur": "Duré",
+                    "name": "Seconde (sec)",
+                    "shortname": "sec"
+                },
+                {
+                    "coef": 60,
+                    "grandeur": "Duré",
+                    "name": "Minute (min)",
+                    "shortname": "min"
+                },
+                {
+                    "coef": 3600,
+                    "grandeur": "Duré",
+                    "name": "Heure (h)",
+                    "shortname": "h"
+                },
+                {
+                    "coef": 86400,
+                    "grandeur": "Duré",
+                    "name": "Jour (j)",
+                    "shortname": "j"
+                },
+                {
+                    "coef": 920160000,
+                    "grandeur": "Duré",
+                    "name": "Mois (mois)",
+                    "shortname": "mois"
+                },
+                {
+                    "coef": 11041920000,
+                    "grandeur": "Duré",
+                    "name": "Année (an)",
+                    "shortname": "an"
+                }
+            ]
+        },
+        {
+            "key": "Ener",
+            "label": "Energie (wh, J, cal...)",
+            "units": [
+                {
+                    "coef": 0.23923445,
+                    "grandeur": "Ener",
+                    "name": "Joule (J)",
+                    "shortname": "J"
+                },
+                {
+                    "coef": 0.23923445,
+                    "grandeur": "Ener",
+                    "name": "Watt-Seconde (ws)",
+                    "shortname": "ws"
+                },
+                {
+                    "coef": 1,
+                    "grandeur": "Ener",
+                    "name": "calorie (cal)",
+                    "shortname": "cal"
+                },
+                {
+                    "coef": 861.24402,
+                    "grandeur": "Ener",
+                    "name": "Watt-Heure (wh)",
+                    "shortname": "wh"
+                },
+                {
+                    "coef": 1000,
+                    "grandeur": "Ener",
+                    "name": "Kilo-Calorie (kcal)",
+                    "shortname": "kcal"
+                },
+                {
+                    "coef": 861244.02,
+                    "grandeur": "Ener",
+                    "name": "KiloWatt-Heure (kwh)",
+                    "shortname": "kwh"
+                },
+                {
+                    "coef": 1000000,
+                    "grandeur": "Ener",
+                    "name": "Mega-calorie (Mcal)",
+                    "shortname": "Mcal"
+                }
+            ]
+        },
+        {
+            "key": "Long",
+            "label": "Longueur (mm, m, km...)",
+            "units": [
+                {
+                    "coef": 0.001,
+                    "grandeur": "Long",
+                    "name": "Millimètre (mm)",
+                    "shortname": "mm"
+                },
+                {
+                    "coef": 0.01,
+                    "grandeur": "Long",
+                    "name": "Centimètre (cm)",
+                    "shortname": "cm"
+                },
+                {
+                    "coef": 1,
+                    "grandeur": "Long",
+                    "name": "Mètre (m)",
+                    "shortname": "m"
+                },
+                {
+                    "coef": 1000,
+                    "grandeur": "Long",
+                    "name": "Kilo-mètre (km)",
+                    "shortname": "km"
+                }
+            ]
+        },
+        {
+            "key": "Mass",
+            "label": "Masse (g, kg...)",
+            "units": [
+                {
+                    "coef": 0.001,
+                    "grandeur": "Mass",
+                    "name": "Milligramme (mg)",
+                    "shortname": "mg",
+                },
+                {
+                    "coef": 1,
+                    "grandeur": "Mass",
+                    "name": "Gramme (g)",
+                    "shortname": "g",
+                },
+                {
+                    "coef": 1000,
+                    "grandeur": "Mass",
+                    "name": "Kilo-gramme (kg)",
+                    "shortname": "kg",
+                },
+                {
+                    "coef": 1000000,
+                    "grandeur": "Mass",
+                    "name": "Tonne (t)",
+                    "shortname": "t",
+                },
+                {
+                    "coef": 1000000000,
+                    "grandeur": "Mass",
+                    "name": "Mega-tonne (Mt)",
+                    "shortname": "Mt"
                 }
             ]
         },
@@ -82,109 +190,19 @@ getGrandeurSpec.res = {
                     "coef": 1,
                     "grandeur": "Nomb",
                     "name": "",
-                    "shortname": "count"
-                }
-            ]
+                    "shortname": "count",
+                },
+            ],
         },
         {
-            "key": "Volu",
-            "label": "Volume (L, m3...)",
+            "key": "Prix",
+            "label": "Prix/Coût (€...)",
             "units": [
                 {
                     "coef": 1,
-                    "grandeur": "Volu",
-                    "name": "mètre-cube",
-                    "shortname": "m3"
-                },
-                {
-                    "coef": 0.001,
-                    "grandeur": "Volu",
-                    "name": "litre",
-                    "shortname": "L"
-                },
-                {
-                    "coef": 20,
-                    "grandeur": "Volu",
-                    "name": "goutte",
-                    "shortname": "goutte"
-                }
-            ]
-        },
-        {
-            "key": "Duré",
-            "label": "Durée (sec, min, h...)",
-            "units": [
-                {
-                    "coef": 1,
-                    "grandeur": "Duré",
-                    "name": "seconde",
-                    "shortname": "sec"
-                },
-                {
-                    "coef": 60,
-                    "grandeur": "Duré",
-                    "name": "minute",
-                    "shortname": "min"
-                },
-                {
-                    "coef": 3600,
-                    "grandeur": "Duré",
-                    "name": "heure",
-                    "shortname": "h"
-                },
-                {
-                    "coef": 86400,
-                    "grandeur": "Duré",
-                    "name": "jour",
-                    "shortname": "j"
-                },
-                {
-                    "coef": 920160000,
-                    "grandeur": "Duré",
-                    "name": "mois",
-                    "shortname": "mois"
-                },
-                {
-                    "coef": 11041920000,
-                    "grandeur": "Duré",
-                    "name": "année",
-                    "shortname": "an"
-                }
-            ]
-        },
-        {
-            "key": "Mass",
-            "label": "Masse (g, kg...)",
-            "units": [
-                {
-                    "coef": 0.001,
-                    "grandeur": "Mass",
-                    "name": "milligramme",
-                    "shortname": "mg"
-                },
-                {
-                    "coef": 1,
-                    "grandeur": "Mass",
-                    "name": "gramme",
-                    "shortname": "g"
-                },
-                {
-                    "coef": 1000,
-                    "grandeur": "Mass",
-                    "name": "kilogramme",
-                    "shortname": "kg"
-                },
-                {
-                    "coef": 1000000,
-                    "grandeur": "Mass",
-                    "name": "tonne",
-                    "shortname": "t"
-                },
-                {
-                    "coef": 1000000000,
-                    "grandeur": "Mass",
-                    "name": "mega-tonne",
-                    "shortname": "Mt"
+                    "grandeur": "Prix",
+                    "name": "euro (€)",
+                    "shortname": "€"
                 }
             ]
         },
@@ -195,56 +213,38 @@ getGrandeurSpec.res = {
                 {
                     "coef": 1,
                     "grandeur": "Surf",
-                    "name": "mètre-carré",
-                    "shortname": "m2"
+                    "name": "Mètre-carré (m2)",
+                    "shortname": "m2",
                 },
                 {
                     "coef": 10000,
                     "grandeur": "Surf",
-                    "name": "hectare",
-                    "shortname": "hec"
+                    "name": "hectare (hec)",
+                    "shortname": "hec",
                 }
             ]
         },
         {
-            "key": "Long",
-            "label": "Longueur (mm, m, km...)",
+            "key": "Volu",
+            "label": "Volume (L, m3...)",
             "units": [
                 {
                     "coef": 0.001,
-                    "grandeur": "Long",
-                    "name": "millimètre",
-                    "shortname": "mm"
-                },
-                {
-                    "coef": 0.01,
-                    "grandeur": "Long",
-                    "name": "centimètre",
-                    "shortname": "cm"
+                    "grandeur": "Volu",
+                    "name": "Litre (L)",
+                    "shortname": "L",
                 },
                 {
                     "coef": 1,
-                    "grandeur": "Long",
-                    "name": "mètre",
-                    "shortname": "m"
+                    "grandeur": "Volu",
+                    "name": "Mètre-cube (m3)",
+                    "shortname": "m3",
                 },
                 {
-                    "coef": 1000,
-                    "grandeur": "Long",
-                    "name": "kilomètre",
-                    "shortname": "km"
-                }
-            ]
-        },
-        {
-            "key": "Prix",
-            "label": "Prix/Coût (€...)",
-            "units": [
-                {
-                    "coef": 1,
-                    "grandeur": "Prix",
-                    "name": "euro",
-                    "shortname": "€"
+                    "coef": 20,
+                    "grandeur": "Volu",
+                    "name": "Goutte",
+                    "shortname": "goutte"
                 }
             ]
         }

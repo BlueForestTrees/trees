a3d009dd267b507959a1ad132574ebbe2b6ec262
slim <smedini@gmail.com>
2018-04-27 17:34:52 +0200

    add facet dialog à recetter

--- src/test/database/facetEntries.js
+++ src/test/database/facetEntries.js
@@ -6,7 +6,7 @@ export const vitBFacetEntry = withEntry("5a6a03c03e77667641d2d2c5", "vitamine B"
 export const vitCFacetEntry = withEntry("5a6a03c03e77667641d2d2c4", "vitamine C", "Dens");
 export const vitDFacetEntry = withEntry("5a6a03c03e77667641d2d2c7", "vitamine D", "Dens");
 export const vitEFacetEntry = withEntry("5a6a03c03e77667641d2d2c8", "vitamine E", "Dens");
-export const prixFacetEntry = withEntry("5a6a03c03e77667641d2d2c6", "Prix", "Coût");
+export const prixFacetEntry = withEntry("5a6a03c03e77667641d2d2c6", "Prix", "Prix");
 
 export const database = {
     [cols.FACET_ENTRY]: [vitDFacetEntry, vitEFacetEntry, vitCFacetEntry, vitBFacetEntry, prixFacetEntry]

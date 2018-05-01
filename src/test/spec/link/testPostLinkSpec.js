293123affdab98219440c27682485a53a94931b5
slim <smedini@gmail.com>
2018-05-01 23:07:01 +0200

    simplifier link (plus que le put)

--- src/test/spec/link/testPostLinkSpec.js
+++ src/test/spec/link/testPostLinkSpec.js
@@ -2,40 +2,16 @@ import {bleToFarineAddSpec, existingRootPostSpec, newRootSpec} from "../root/tes
 import {existingBranchPostSpec, farineToBleBranchAddSpec, newBranchSpec} from "../branch/testPostBranchSpec";
 
 export const newLinkSpec = {};
-newLinkSpec.req = {
-    body: newRootSpec.req.body
-};
-newLinkSpec.res = {
-    body: [newRootSpec.res.body, newBranchSpec.res.body]
-};
-newLinkSpec.db = {
-    expected: {
-        list: [newRootSpec.db.expected, newBranchSpec.db.expected]
-    }
-};
+newLinkSpec.req = {body: newRootSpec.req.body};
+newLinkSpec.res = {body: [newRootSpec.res.body, newBranchSpec.res.body]};
+newLinkSpec.db = {expected: {list: [newRootSpec.db.expected, newBranchSpec.db.expected]}};
 
 export const existingLinkPostSpec = {};
-existingLinkPostSpec.req = {
-    body: existingRootPostSpec.req.body
-};
-existingLinkPostSpec.res = {
-    body: [existingRootPostSpec.res.body, existingBranchPostSpec.res.body]
-};
-existingLinkPostSpec.db = {
-    expected: {
-        list: [existingRootPostSpec.db.expected, existingBranchPostSpec.db.expected]
-    }
-};
+existingLinkPostSpec.req = {body: existingRootPostSpec.req.body};
+existingLinkPostSpec.res = {body: [existingRootPostSpec.res.body, existingBranchPostSpec.res.body]};
+existingLinkPostSpec.db = {expected: {list: [existingRootPostSpec.db.expected, existingBranchPostSpec.db.expected]}};
 
 export const bleToFarineLinkAddSpec = {};
-bleToFarineLinkAddSpec.req = {
-    body: bleToFarineAddSpec.req.body
-};
-bleToFarineLinkAddSpec.res = {
-    body: [bleToFarineAddSpec.res.body, farineToBleBranchAddSpec.res.body]
-};
-bleToFarineLinkAddSpec.db = {
-    expected: {
-        list: [bleToFarineAddSpec.db.expected, farineToBleBranchAddSpec.db.expected]
-    }
-};
+bleToFarineLinkAddSpec.req = {body: bleToFarineAddSpec.req.body};
+bleToFarineLinkAddSpec.res = {body: [bleToFarineAddSpec.res.body, farineToBleBranchAddSpec.res.body]};
+bleToFarineLinkAddSpec.db = {expected: {list: [bleToFarineAddSpec.db.expected, farineToBleBranchAddSpec.db.expected]}};

a2cea4775f3750a2f3dd30a4675b2bc419da8b10
slim <smedini@gmail.com>
2018-10-05 19:55:13 +0200

    facets off import + de upsert mais pas fini trunk externId index

--- src/dbRegistry.js
+++ src/dbRegistry.js
@@ -6,5 +6,10 @@ export const registry = [
         version: "0.0.3",
         log: "ImpactEntry.externId is unique",
         script: () => col(cols.IMPACT_ENTRY).createIndex({"externId": 1}, {unique: true, partialFilterExpression: {externId: {$exists: true}}})
+    },
+    {
+        version: "1.0.6",
+        log: "index: TrunkCollection.externId",
+        script: () => col(cols.TRUNK).createIndex({"externId":1})
     }
 ]
